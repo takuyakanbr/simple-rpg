d50edf8790b024f8dea8532c9b81ab7c3afcdb36
Daniel Teo <takuyakanbr@gmail.com>
2017-08-09 19:20:52 +0800

    Initial commit

---
+++ Engine/InventoryItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class InventoryItem : INotifyPropertyChanged
+    {
+        private Item _details;
+        private int _quantity;
+
+        public Item Details
+        {
+            get { return _details; }
+            set
+            {
+                _details = value;
+                OnPropertyChanged("Details");
+            }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged("Quantity");
+                OnPropertyChanged("Description");
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return Quantity > 1 ? Details.NamePlural :
+                Details.Name;
+            }
+        }
+        public int ItemID
+        {
+            get { return Details.ID; }
+        }
+        public int Price
+        {
+            get { return Details.Price; }
+        }
+
+        public InventoryItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class LivingCreature : INotifyPropertyChanged
+    {
+        private int _currentHitPoints;
+        public int CurrentHitPoints
+        {
+            get { return _currentHitPoints; }
+            set
+            {
+                _currentHitPoints = value;
+                OnPropertyChanged("CurrentHitPoints");
+            }
+        }
+        public int MaximumHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
